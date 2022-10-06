0715645b6da68f5536c5baa58b36674d6b04a603
Langston Barrett <langston.barrett@gmail.com>
2022-10-06 17:22:26 -0400

    datalog: Implement import*.dl via include
    Now the FactGenerator and Datalog code have the same source of truth about
    relation and file names.

--- FactGenerator/src/predicate_groups.cpp
+++ FactGenerator/src/predicate_groups.cpp
@@ -1,13 +1,15 @@
 #include "predicate_groups.hpp"
 using namespace cclyzer::predicates;
 
-// GROUP_BEGIN, GROUP_END not needed
-//
 // Try: gcc -I include -E src/predicate_groups.cpp
+//
+// GROUP_BEGIN, GROUP_END not needed
 #define GROUP_BEGIN(g)
 #define GROUP_END(group_end)
-// TODO(lb): Derive filename from group and predicate, e.g.,
+// TODO(#42): Derive relation/file name from group + predicate name, e.g.,
 // #define PREDICATE(g, p, f) pred_t g::p (std::string(#g) + std::string("_") +
 // std::string(#p));
+#define Q(x) #x
+#define QUOTE(x) Q(x)
 #define PREDICATE(g, p, f) pred_t g::p(#f);
 #include "predicates.inc"
