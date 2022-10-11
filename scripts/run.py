560dc8e8939fe719055535c643fe7de1e49bcccd
Langston Barrett <langston.barrett@gmail.com>
2022-10-11 18:30:54 -0400

    Further improvements to diff script

--- scripts/run.py
+++ scripts/run.py
@@ -1,6 +1,24 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python3
 
-"""Helper to run cclyzer++ and produce human-readable output."""
+"""Helper to run cclyzer++ and produce human-readable output.
+
+Especially useful for diffing output between two cclyzer++ revisions to see
+what has changed. Example use:
+
+    alias ccl-dev-run='docker run --rm -it --mount type=bind,src=$PWD,target=/work --workdir=/work clyzer-dev'
+    rm -rf build
+
+    git checkout <old>
+    ccl-dev-run cmake -G Ninja -B build -S . -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+    ccl-dev-run cmake --build build -j $(nproc)
+    git checkout main scripts/run.py
+    ccl-dev-run python3 scripts/run.py prog.ll old
+
+    git checkout <new>
+    ccl-dev-run cmake -G Ninja -B build -S . -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+    ccl-dev-run cmake --build build -j $(nproc)
+    ccl-dev-run python3 scripts/run.py prog.ll new old
+"""
 
 from __future__ import annotations
 
@@ -47,25 +65,60 @@ def normalize(out: Path) -> None:
         uncompressed = relation_path.with_suffix("")
         with open(uncompressed, "w") as out_file:
             with gzip.open(relation_path, "rt") as in_file:
-                for line in sorted(in_file.readlines()):
+                # Unfortunately, the FactGenerator outputs relations with
+                # duplicates.
+                lines = sorted(set(in_file.readlines()))
+                for line in lines:
                     out_file.write(line)
+
+
+# These relations have been renamed at some point in the past, so when
+# comparing outputs all of them must be considered.
+RENAMED = {
+    frozenset({
+        "add_instruction",
+        "add_instr"
+    })
+}
+
+
+def compare_rel(relation_path: Path, compare_path: Path) -> None:
+    if not compare_path.exists():
+        return
+    if relation_path.name.startswith("unification"):
+        # Explicitly non-deterministic
+        return
+    if relation_path.suffix != ".csv":
+        return
+    same = filecmp.cmp(str(relation_path), str(compare_path))
+    if not same:
+        print(f"{relation_path} differs from {compare_path}")
+        run(["diff", "--unified", str(relation_path), str(compare_path)])
 
 
 def compare_dirs(out: Path, compare: Path) -> None:
     assert out != compare
+    out_rels = list(out.iterdir())
+    cmp_rels = list(compare.iterdir())
+    if len(out_rels) != len(cmp_rels):
+        # In this case, this script might not output all differences.
+        print("[WARN] Different number of relations")
     for relation_path in out.iterdir():
-        if relation_path.name.startswith("unification"):
-            # Explicitly non-deterministic
-            continue
-        if relation_path.suffix != ".csv":
-            continue
         compare_path = compare / relation_path.name
-        same = filecmp.cmp(str(relation_path), str(compare_path))
-        if not same:
-            print(f"{relation_path} differs from {compare_path}")
-            run(["diff", "--unified", str(relation_path), str(compare_path)])
-
-
+        compare_rel(relation_path, compare_path)
+        for names in RENAMED:
+            if relation_path.name in names:
+                for name in names:
+                    compare_path = compare / name
+                    compare_rel(relation_path, compare_path)
+    for relation_path in compare.iterdir():
+        compare_path = out / relation_path.name
+        compare_rel(relation_path, compare_path)
+        for names in RENAMED:
+            if relation_path.name in names:
+                for name in names:
+                    compare_path = out / name
+                    compare_rel(relation_path, compare_path)
 
 
 def main(argv: List[str]) -> None:
