3de1e390ffc9efab268a9fec3894f9f1bec71346
Langston Barrett <langston.barrett@gmail.com>
2022-10-21 14:06:06 -0400

    ci, cmake: Run clang-tidy in CI

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,6 +6,13 @@ for f in ./src/*.cpp ./FactGenerator/**/*.cpp; do
   clang-format-10 "${f}" | diff "${f}" -
 done
 
+if ! [[ -f build/compile_commands.json ]]; then
+  printf "Run cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -G Ninja -S . -B build.\n"
+  exit 1
+fi
+
+cmake --build build -j "$(nproc)" --target factgen-tidy
+
 cd test
 mypy .
 mypy ../scripts/*.py
