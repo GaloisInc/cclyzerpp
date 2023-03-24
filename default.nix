74e045e20a414c6ee993e8b5d66a30d0dcdfa3f5
Giulio De Pasquale <git@depasquale.giugl.io>
2023-03-24 15:25:20 -0700

    flake: Added rudimentary flake

---
+++ default.nix
@@ -0,0 +1,58 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+, llvmPackages_14
+, boost
+, souffle
+, useUBSAN ? true
+, buildInterface ? false
+}:
+
+let
+  inherit (llvmPackages_14) llvm;
+in
+stdenv.mkDerivation rec {
+  pname = "cclyzerpp";
+  version = "0.7.0";
+
+  src = fetchFromGitHub {
+    owner = "GaloisInc";
+    repo = "cclyzerpp";
+    rev = "v${version}";
+    hash = "sha256-mEL/Lv4RngD754PpKClPYk4I2U9PBcdI6FIhBv+d9Ms=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    llvm
+    boost
+    souffle
+  ];
+
+  buildInputs = [
+    souffle
+  ];
+
+  CMAKE_C_FLAGS = lib.optional useUBSAN [ "-DUBSAN=1" ];
+  
+  buildPhase = ''
+    cmake --build . -- factgen-exe ${lib.optionalString buildInterface "PAPass" }
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+
+    # could not build PAPass, no output
+    cp factgen-exe $out
+
+    # we should use this to install
+    # cmake --install .
+  '';
+  
+  meta = with lib; {
+    description = "Cclyzer++ is a precise and scalable global pointer analysis for LLVM code";
+    homepage = "https://github.com/GaloisInc/cclyzerpp/tree/main";
+    license = licenses.bsd3;
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        inherit (pkgs) callPackage;
+      in
+      {
+        defaultPackage = callPackage ./default.nix { };
+      });
+}
+
+
