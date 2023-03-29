2f0100820cdfb707da5dc1c42a862ef345577822
Giulio De Pasquale <git@depasquale.giugl.io>
2023-03-30 00:27:47 +0200

    nix: Clean-up derivation. Remove custom steps in phases as now we rely on Nix to invoke CMake.

--- default.nix
+++ default.nix
@@ -1,56 +1,33 @@
 { lib
-, stdenv
 , fetchFromGitHub
 , cmake
 , llvmPackages_14
 , boost
 , souffle
-, useUBSAN ? true
-, buildInterface ? false
+, ninja
 }:
 
 let
-  inherit (llvmPackages_14) llvm;
+  inherit (llvmPackages_14) llvm stdenv openmp;
 in
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "cclyzerpp";
   version = "0.7.0";
 
-  src = fetchFromGitHub {
-    owner = "GaloisInc";
-    repo = "cclyzerpp";
-    rev = "v${version}";
-    hash = "sha256-mEL/Lv4RngD754PpKClPYk4I2U9PBcdI6FIhBv+d9Ms=";
-  };
+  src = ./.;
 
   nativeBuildInputs = [
     cmake
     llvm
     boost
     souffle
+    openmp
   ];
 
   buildInputs = [
     souffle
   ];
 
-  CMAKE_C_FLAGS = lib.optional useUBSAN [ "-DUBSAN=1" ];
-  
-  buildPhase = ''
-    cmake --build . -- factgen-exe ${lib.optionalString buildInterface "PAPass" }
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin
-
-    # could not build PAPass, no output
-    cp factgen-exe $out/bin
-    cp -r ../datalog $out
-
-    # we should use this to install
-    # cmake --install .
-  '';
-  
   meta = with lib; {
     description = "cclyzer++ is a precise and scalable global pointer analysis for LLVM code";
     homepage = "https://github.com/GaloisInc/cclyzerpp/tree/main";
