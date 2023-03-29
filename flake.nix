cd8002957cdc395f6b426b13b8c160cb621d3f4b
Giulio De Pasquale <git@depasquale.giugl.io>
2023-03-30 00:35:07 +0200

    flake: Pin to nixpkgs 22.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
